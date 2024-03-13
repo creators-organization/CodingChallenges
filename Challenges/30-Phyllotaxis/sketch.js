3ba3205b12f1a9937388627ab29dbc41878e0d02
Stephen Brasel <stephen.brasel@gmail.com>
2024-03-12 23:01:16 -0400

    feat(Challenges) adds hue shifting to Phyllotaxis

--- Challenges/30-Phyllotaxis/sketch.js
+++ Challenges/30-Phyllotaxis/sketch.js
@@ -2,10 +2,13 @@ var n = 0;
 var c = 15;
 var angle = 137.5;
 var maxPetals = 10000;
-var petalSize = 120;
+var petalSize = 100;
 var rotateSpeed = 0.001;
+var hueShift = 0;
+var hueShiftSpeed = 0.5;
 
 function setup(){
+	// createCanvas(600, 600);
 	createCanvas(windowWidth, windowHeight);
 	angleMode(DEGREES);
 	colorMode(HSB);
@@ -16,17 +19,19 @@ function draw(){
 	translate(width*0.5, height*0.5);
 	for (let i = 0; i < maxPetals; i++) {
 		var a = i * (map(sin(angle), -1, 1, 0, 360));
+		// var a = i * angle;
 		var r = c * sqrt(i);
 		var x = r * cos(a);
 		var y = r * sin(a);
 	
 		// fill(a % 360, 255, 255);
 		// fill(i % 360, 255, 255);
-		fill((a - r) % 360, 255, 255);
+		fill((hueShift + a - r) % 360, 255, 255);
 		noStroke();
 		ellipse(x, y, petalSize, petalSize);
 	}
-	angle += rotateSpeed;
+	angle = (angle + rotateSpeed) % 360;
+	hueShift = (hueShift + hueShiftSpeed) % 360;
 }
 
 function windowResized() {
